c3f6604a7df59f01989827171764c2970dce2fde
Jacob Norwood <jacobnorwood92@gmail.com>
2022-02-09 19:30:56 -0600

    formatting

--- src/lib/Table/Game.svelte
+++ src/lib/Table/Game.svelte
@@ -99,26 +99,25 @@
   let currentBet = 0
 
   const sortedPlayers = players.sort((a, b) => {
-    // sort by player.seat
     if (a.seat < b.seat) {
       return -1
     }
   })
   const earlySeats = sortedPlayers.filter(player => player.seat < self.seat)
   const lateSeats = sortedPlayers.filter(player => player.seat >= self.seat)
-  const playersWithSeats = [ ...lateSeats, ...earlySeats]
+  const playersWithSeats = [...lateSeats, ...earlySeats]
 </script>
 
 <div>
-<div class="game-table">
-  {#each playersWithSeats as player, index}
-    {#if index < 2}
-      <Player {player} right visualPosition={index}  />
-    {:else}
-      <Player {player} visualPosition={index} />
-    {/if}
-  {/each}
-</div>
+  <div class="game-table">
+    {#each playersWithSeats as player, index}
+      {#if index < 2}
+        <Player {player} right visualPosition={index} />
+      {:else}
+        <Player {player} visualPosition={index} />
+      {/if}
+    {/each}
+  </div>
 
   <Hand />
   <Controls minBet={0} maxBet={self.chipCount} betSize={currentBet} player={self} />
