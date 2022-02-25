1ceaebef02050ac053e3344f688b3d5230caa48a
Jacob Norwood <jacobnorwood92@gmail.com>
2022-02-24 23:27:09 -0600

    minimize calls

--- src/lib/Auth.svelte
+++ src/lib/Auth.svelte
@@ -1,32 +1,23 @@
 <script>
-  import { onMount } from 'svelte'
   import auth from './../services/auth'
-  import { isAuthenticated, user } from './../stores/auth'
+  import { isAuthenticated } from './../stores/auth'
   import Button from '$lib/Button.svelte'
   import { goto } from '$app/navigation'
-  let auth0Client
 
   let loggedIn = $isAuthenticated
 
-  onMount(async () => {
-    auth0Client = await auth.createClient()
-  })
-
   const login = async () => {
+    const auth0Client = await auth.createClient()
     const success = await auth.loginWithPopup(auth0Client)
     if (success) {
-      loggedIn = true
-      isAuthenticated.set(await auth0Client.isAuthenticated())
-      user.set(await auth0Client.getUser())
       goto('/dashboard')
     }
   }
 
   const logout = async () => {
+    const auth0Client = await auth.createClient()
     const success = await auth.logout(auth0Client)
     if (success) {
-      isAuthenticated.set(false)
-      loggedIn = false
       goto('/')
     }
   }
--- src/routes/dashboard.svelte
+++ src/routes/dashboard.svelte
@@ -5,6 +5,7 @@
   import Button from '$lib/Button.svelte'
   import Section from '$lib/Section.svelte'
   import Row from '$lib/Row.svelte'
+  import { user } from "../stores/auth";   
   setContext('currentPage', '/dashboard')
 </script>
 
@@ -17,7 +18,7 @@
 
 <AuthPage>
   <h1 class="text-5xl font-extrabold mb-2">Find a table</h1>
-
+  {$user.name}
   <Row>
     <Section centerText>
       <h2 class="text-2xl font-extrabold mb-2">Heads Up</h2>
